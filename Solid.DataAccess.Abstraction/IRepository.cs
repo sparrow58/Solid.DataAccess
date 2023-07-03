b08b51e4d2aab0129ec1ee9b162c8e71972c8450
a.alsabsab <a.alsabsab@teleyemen.com.ye>
2023-07-04 01:23:03 +0300

    Reorder the folders

---
+++ Solid.DataAccess.Abstraction/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+
+namespace Solid.DataAccess.Abstraction
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        TEntity GetById(object id);
+        void Add(TEntity entity);
+        void UpdateIfNoTracking(TEntity entityToUpdate);
+        void AddRange(IEnumerable<TEntity> entities);
+        Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, string includeProperties = "");
+        Task<TResult> FirstOrDefaultAsync<TResult>(Expression<Func<TEntity, bool>> predicate, Expression<Func<TEntity, TResult>> selector, string includeProperties = "");
+        Task<List<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        ValueTask<TEntity> GetByIdAsync(object id);
+        Task<List<TResult>> GetAsync<TResult>(Expression<Func<TEntity, bool>> predicate, Expression<Func<TEntity, TResult>> selector, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        Task<List<TResult>> GetAsync<TResult>(Expression<Func<TEntity, TResult>> selector, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TResult> MaxAsync<TResult>(Expression<Func<TEntity, TResult>> selector);
+        Task<TResult> MaxAsync<TResult>(Expression<Func<TEntity, bool>> predicate, Expression<Func<TEntity, TResult>> selector);
+        Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<long> CountLongAsync(Expression<Func<TEntity, bool>> predicate);
+        TEntity GetById(params object[] ids);
+        ValueTask<TEntity> GetByIdAsync(params object[] ids);
+    }
+}
---
+++ Solid.DataAccess.Abstraction/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿namespace Solid.DataAccess.Abstraction
+{
+    public interface IUnitOfWork
+    {
+        IQueryable<TEntity> Query<TEntity>() where TEntity : class;
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+        Task<int> CommitAsync(CancellationToken cancellationToken = default);
+        Task<int> ExecuteSqlRawAsync(string sql, params object[] parameters);
+        Task<int> ExecuteSqlInterpolatedAsync(FormattableString sql, CancellationToken cancellationToken = default);
+        void BeginTransaction();
+        void RoleBack();
+        IQueryable<TEntity> QueryNoTracking<TEntity>() where TEntity : class;
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
